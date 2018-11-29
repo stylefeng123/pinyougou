7a1151af289304d85fdb3b644fee8d9bb2005b73
root <root>
2018-11-29 16:44:07 +0800

    111

--- pinyougou-shop-web/target/pinyougou-shop-web-0.0.1-SNAPSHOT/js/controller/indexController.js
+++
@@ -1,8 +0,0 @@
-app.controller('indexController',function($scope,$controller   ,loginService){
-	
-	$scope.showLoginName = function(){
-		loginService.loginName().success(function(data){
-			$scope.loginName=data.loginName;
-		});
-	}
-})
